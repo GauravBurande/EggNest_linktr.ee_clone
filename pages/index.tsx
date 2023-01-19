472fd41fe7642aa3f7515e161af63731c5320b49
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-19 10:27:58 +0530

    completed styling and building home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,14 @@
 import type { NextPage } from 'next'
-import EggNest from '../components/eggnest'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
 
   return (
     <div>
-      <div className='absolute -z-10 h-[100vh] w-[100vw] bg-gray-900'>
-        <img
-          className='w-full h-full object-cover opacity-10 blur-lg'
-          src="/eggnest.png"
-          alt="eggnest"
-        />
+      <div>
+
       </div>
-      <div className='max-w-4xl mx-auto text-white'>
-        <EggNest />
-      </div>
+      <Link href={"/home"}>go to home</Link>
     </div>
   )
 }
