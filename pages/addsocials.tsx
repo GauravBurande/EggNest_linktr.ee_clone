4be5290a7b9448ed5cf5017bffd0b1a7eed4e4a6
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-30 19:10:13 +0530

    chore: modified addsocials page

--- pages/addsocials.tsx
+++ pages/addsocials.tsx
@@ -74,10 +74,6 @@ const AddSocials = () => {
                     </div>
                 </div>
             </div>
-
-            <div className='flex justify-center'>
-                <p className='text-center absolute -bottom-44'>Built with <span className='text-red-500'>❤</span> by <a href="https://twitter.com/gauravvan" className='text-green-300 hover:underline decoration-yellow-300'>@gauravvan</a></p>
-            </div>
         </div>
     )
 }
